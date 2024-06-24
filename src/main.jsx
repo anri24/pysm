6e5ac8dcdb217e31e4f88d8ccb6eacdbeef3a910
anri varshanidze <anrivarshanidze2407@gmail.com>
2024-06-24 06:30:23 +0400

    add missing context in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import i18next from 'i18next'
 import global_en from './locale/en/global.json'
